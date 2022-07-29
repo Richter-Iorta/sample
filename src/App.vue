29c2b69958844b627f22634e81d98ef07373f236
Richter-Iorta <richter.anto@iorta.in>
2022-07-29 16:28:18 +0530

    sample3

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Sample />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Sample from './components/sample.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    Sample
+}
 }
 </script>
 
